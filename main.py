996486dc92cb0f9b92dce86b34b582a01493a0ba
pranavgorantla <111912611+pranavgorantla@users.noreply.github.com>
2022-10-24 20:08:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-Math = [100, 90]
-English = [90, 80]
-Physics = [80, 80]
-Computer = [80, 90]
-x = np.arange(2) 
-width = 0.2
-labels = ['Math', 'English', 'Physics', 'Computer']
-names = ['Bill', 'Mary']
+width = 0.15
+x = np.arange(2)
+Math = [100,90]
+English = [90,80]
+Physics = [80,80]
+Computer = [90,90]
 
-# ******************************
-# Make your code
-# ******************************
+fig,ax = plt.subplots()
+math = ax.bar(x-width*1.5,Math,width)
+eng = ax.bar(x-width*.5,English,width)
+phy = ax.bar(x+width*1.5,Physics,width)
+com = ax.bar(x+width*.5,Computer,width)
 
+ax.legend(["Math","English","Physics","Computer"],loc = "lower center")
 
+ax.set_title("Grouped graph for scores")
+
+ax.bar_label(math,Math)
+ax.bar_label(eng,English)
+ax.bar_label(phy,Physics)
+ax.bar_label(com,Computer)
+
+#ax.set_xticks()
+ax.set_xticks(ticks = [0,1])
+ax.set_xticklabels(["Billy","Mary"])
 fig.savefig('A11.png')
